c520d3610c4a1c842f835001a176b0050ef94567
Tyron Wytrykowski <tyronwyt@gmail.com>
2019-01-03 00:15:12 +0000

    add login route, view and styles

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var sassMiddleware = require('node-sass-middleware');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var itineraryRouter = require('./routes/itinerary');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/itinerary', itineraryRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
